50a906d810ecc435cc67f9435c1fb3940d06ba56
Alex-Wzq <wzq_alex@163.com>
2023-04-12 23:37:05 +0800

    finish

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 
-axios.defaults.baseURL = "http://localhost:8081"
+axios.defaults.baseURL = "http://localhost:8091"
 
 // 前置拦截
 axios.interceptors.request.use(config => {
@@ -12,33 +12,33 @@ axios.interceptors.request.use(config => {
 })
 
 axios.interceptors.response.use(response => {
-    let res = response.data;
+  let res = response.data;
 
-    console.log("=================")
-    console.log(res)
-    console.log("=================")
+  console.log("=================")
+  console.log(res)
+  console.log("=================")
 
-    if (res.code === 200) {
-      return response
-    } else {
+  if (res.code === 200) {
+    return response
+  } else {
 
-      Element.Message.error('错了哦，这是一条错误消息', {duration: 3 * 1000})
+    Element.Message.error('错了哦，这是一条错误消息', { duration: 3 * 1000 })
 
-      return Promise.reject(response.data.msg)
-    }
-  },
+    return Promise.reject(response.data.msg)
+  }
+},
   error => {
     console.log(error)
-    if(error.response.data) {
+    if (error.response.data) {
       error.message = error.response.data.msg
     }
 
-    if(error.response.status === 401) {
+    if (error.response.status === 401) {
       store.commit("REMOVE_INFO")
       router.push("/login")
     }
 
-    Element.Message.error(error.message, {duration: 3 * 1000})
+    Element.Message.error(error.message, { duration: 3 * 1000 })
     return Promise.reject(error)
   }
 )
